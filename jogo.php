d5baf15655a5239dbbff389c6c49bfb66e96ae61
Igor-Jadson <111446489+Igor-Jadson@users.noreply.github.com>
2022-08-17 12:48:05 -0300

    Add files via upload

---
+++ jogo.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz - Copa do Mundo 2022</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        html, body {
+            width: 100%;
+            height: 100%;
+        }
+
+        body {
+            background: #f5f5f5;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .container {
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            padding: 20px;
+            background: #fff;
+            max-width: 450px;
+            min-height: 300px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Quiz - Copa do Mundo 2022 - Seleção Brasileira</h1>
+
+    </div>
+</body>
+</html>
